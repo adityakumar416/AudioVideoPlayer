2b0772078856d6cdbfdb114c792005b1e08c3b7b
macBook <Aditya0683624@gmail.com>
2023-05-23 11:16:08 +0530

    Audio Player is in Progress.

--- app/src/main/java/com/example/miniplayerapp/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/miniplayerapp/AudioPlayerActivity.kt
@@ -1,13 +1,26 @@
 package com.example.miniplayerapp
 
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.miniplayerapp.databinding.ActivityAudioPlayerBinding
+import com.example.miniplayerapp.databinding.ActivityVideoPlayerBinding
 
 class AudioPlayerActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityAudioPlayerBinding
+    private lateinit var mediaPlayer: MediaPlayer
+    private var isPlaying = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_audio_player)
+        binding = ActivityAudioPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mediaPlayer = MediaPlayer()
+        binding.selectAudio.setOnClickListener {
+
+        }
+
     }
 }
