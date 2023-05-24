2ff297829862d4a1421acdebd5909bef70f3e4a1
macBook <Aditya0683624@gmail.com>
2023-05-24 11:46:48 +0530

    Audio Player is in Progress.

--- app/src/main/java/com/example/miniplayerapp/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/miniplayerapp/AudioPlayerActivity.kt
@@ -5,6 +5,7 @@ import android.media.MediaPlayer
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import com.example.miniplayerapp.databinding.ActivityAudioPlayerBinding
 import com.example.miniplayerapp.databinding.ActivityVideoPlayerBinding
 
@@ -19,8 +20,51 @@ class AudioPlayerActivity : AppCompatActivity() {
 
         mediaPlayer = MediaPlayer()
         binding.selectAudio.setOnClickListener {
-
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "audio/*"
+            startActivityForResult(intent,1)
+        }
+        binding.playPause.setOnClickListener {
+            if (isPlaying) {
+                mediaPlayer.pause()
+                binding.playPause.text = "play"
+                isPlaying = false
+            }
+            else{
+                mediaPlayer.start()
+                binding.playPause.text = "pause"
+                isPlaying = true
+            }
+        }
+        mediaPlayer.setOnPreparedListener {
+            mediaPlayer.start()
+            binding.playPause.text = "pause"
+            isPlaying = true
         }
 
+        mediaPlayer.setOnCompletionListener {
+            isPlaying = false
+            binding.playPause.text = "play"
+        }
+
+
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            val uri = data?.data?:return
+            mediaPlayer.reset()
+            mediaPlayer.setDataSource(applicationContext,uri)
+            mediaPlayer.prepare()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
 }
--- app/src/main/java/com/example/miniplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/miniplayerapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.miniplayerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.miniplayerapp.databinding.ActivityAudioPlayerBinding
+import com.example.miniplayerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.video.setOnClickListener {
+            val intent = Intent(this,VideoPlayerActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        binding.audio.setOnClickListener {
+            val intent = Intent(this,AudioPlayerActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
